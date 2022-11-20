0ecf2dd935660ba8b28720b3f5e7ff44e3891954
i <irinainani@gmail.com>
2022-11-20 17:11:19 +0700

    added scroll

---
+++ script.js
@@ -0,0 +1,36 @@
+const articleKnowMore = document.querySelector(".know_more");
+const articleService = document.querySelector(".services");
+const articlePortfolio = document.querySelector(".portfolio");
+// const articleContact = document.querySelector(".contact");
+
+const btnKnowMore = document.querySelector("button");
+
+const about = document.querySelector("a[href='#about']");
+const service = document.querySelector("a[href='#service']");
+const portfolio = document.querySelector("a[href='#portfolio']");
+// const contact = document.querySelector("a[href='#contact']");
+
+function scrollArticle() {
+  articleKnowMore.scrollIntoView({ block: "center", behavior: "smooth" });
+}
+
+btnKnowMore.addEventListener("click", scrollArticle);
+about.addEventListener("click", scrollArticle);
+
+function scrollService() {
+  articleService.scrollIntoView({ block: "center", behavior: "smooth" });
+}
+
+service.addEventListener("click", scrollService);
+
+function scrollPortfolio() {
+  articlePortfolio.scrollIntoView({ block: "center", behavior: "smooth" });
+}
+
+portfolio.addEventListener("click", scrollPortfolio);
+
+// function scrollContact() {
+//   articleContact.scrollIntoView({ block: "center", behavior: "smooth" });
+// }
+
+// contact.addEventListener("click", scrollContact);
